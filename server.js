5690e6327dedb1b13e5ae24b34495bdf456e9e89
rutul089 <rutul089@gmail.com>
2020-03-03 17:04:46 +0530

    conneced to mongodb and add model file

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const logger = require("./middleware/logger"); // -- Custom logger
-
-// Route files
-const locations = require("./routes/locations");
-
+const connectDB = require("./config/db");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+// Connect to db
+connectDB();
+// Route files
+const locations = require("./routes/locations");
 
 const app = express();
 
@@ -22,7 +23,16 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/locations", locations);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mod on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mod on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`.red.bold);
+  // Close server
+  server.close(() => process.exit(1));
+});
