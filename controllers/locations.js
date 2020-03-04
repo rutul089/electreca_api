9fb700706c25ae592283f59718c42499240574f3
rutul089 <rutul089@gmail.com>
2020-03-04 17:01:50 +0530

    Implementing CRUD operation for Locatino module

--- controllers/locations.js
+++ controllers/locations.js
@@ -3,17 +3,55 @@ const Location = require("../models/Location");
 // @desc    Get all locations
 // @route   GET /api/v1/locations
 // @access  Private
-exports.getLocations = (req, res, next) => {
-  res.status(200).json({ sucess: true, msg: "Show all locations" });
+exports.getLocations = async (req, res, next) => {
+  try {
+    const location = await Location.find({}, { __v: 0 });
+    res.status(200).json({
+      isSucess: true,
+      errorCode: 0,
+      errorMessage: "Ok",
+      count: location.length,
+      Data: location
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      isSucess: false,
+      errorCode: 400,
+      errorMessage: "Something went wrong!"
+    });
+  }
 };
 
 // @desc    Get single locations
 // @route   GET /api/v1/locations/:id
 // @access  Private
-exports.getLocation = (req, res, next) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Display location ${req.params.id}` });
+exports.getLocation = async (req, res, next) => {
+  // --
+  try {
+    const location = await Location.findById(req.params.id, { __v: 0 });
+    if (!location) {
+      // it will check that id is exist or not
+      return res.status(200).json({
+        isSucess: false,
+        errorCode: 101,
+        errorMessage: "No data found"
+      });
+    }
+    res.status(200).json({
+      isSucess: true,
+      errorCode: 0,
+      errorMessage: "Ok",
+      Data: location
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      isSucess: false,
+      errorCode: 400,
+      errorMessage: "Something went wrong!"
+    });
+  }
 };
 
 // @desc    Create new locaton
@@ -31,7 +69,7 @@ exports.createLocation = async (req, res, next) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(200).json({
+    res.status(400).json({
       isSucess: false,
       errorCode: 400,
       errorMessage: "Something went wrong!"
@@ -42,17 +80,61 @@ exports.createLocation = async (req, res, next) => {
 // @desc    Update location
 // @route   PUT /api/v1/locations/:id
 // @access  Private
-exports.updateLocation = (req, res, next) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Update location ${req.params.id}` });
+exports.updateLocation = async (req, res, next) => {
+  try {
+    const location = await Location.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!location) {
+      // it will check that id is exist or not
+      return res.status(200).json({
+        isSucess: false,
+        errorCode: 101,
+        errorMessage: "No data found"
+      });
+    }
+    res.status(200).json({
+      isSucess: true,
+      errorCode: 0,
+      errorMessage: "Data has been updated scusessfully.",
+      Data: location
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      isSucess: false,
+      errorCode: 400,
+      errorMessage: "Something went wrong!"
+    });
+  }
 };
 
 // @desc    Delete location
 // @route   DELETE /api/v1/locations/:id
 // @access  Private
-exports.deleteLocation = (req, res, next) => {
-  res
-    .status(200)
-    .json({ sucess: true, msg: `Delete location ${req.params.id}` });
+exports.deleteLocation = async (req, res, next) => {
+  try {
+    const location = await Location.findOneAndDelete(req.params.id);
+    if (!location) {
+      // it will check that id is exist or not
+      return res.status(200).json({
+        isSucess: false,
+        errorCode: 101,
+        errorMessage: "No data found"
+      });
+    }
+    res.status(200).json({
+      isSucess: true,
+      errorCode: 0,
+      errorMessage: "Data has been deleted sucessfully"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      isSucess: false,
+      errorCode: 400,
+      errorMessage: "Something went wrong!"
+    });
+  }
 };
